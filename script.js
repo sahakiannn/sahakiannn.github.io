204d72b7b41456c1d9065b30fd005bcaeaf2362d
sahakiannn <116956517+sahakiannn@users.noreply.github.com>
2022-11-21 16:15:32 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,110 @@
-/*let a, b;
-a = prompt("input number a = ");
-b = prompt("input number b = ");
-
-alert(+a + +b);
-alert(+a - +b);
-alert(+a * +b);
-alert(+a / +b);
-alert(+a % +b);*/
-
-/*
-let namee, age;
-namee = prompt("What is your name?");
-age = prompt("how old are you?");
-alert(`My name is ${namee}, I am ${age} years old`);
-*/
-
-/*let num = prompt("Enter a number");
-if(num <0 ){
-alert("YES");
-}
- else{
-    alert("NO");
- }*/
-
- /*let num = prompt("Enter a number");
- if(num % 2 == 0 ){
- alert("even");
- }
-  else{
-     alert("odd");
-  }*/
-
- /* let num1, num2;
-  num1 = prompt("Enter a number");
-  num2 = prompt("Enter a number");
-
- if(num1 % num2 === 0 || num2 % num1 === 0){
- alert("1");
- }
-  else{
-     alert("0");
-  }*/
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Homework</title>
+</head>
+<body>
+    <script>
+        let a=11; b=9
+        let sum=a+b; diff=a-b; prod=a*b; quo=a/b; rem=a%b;
+        document.write("Task 1-")
+        document.write(sum+ "\n");
+        document.write(diff+ "\n");
+        document.write(prod+ "\n");
+        document.write(quo+ "\n");
+        document.write(rem+ "\n");
+        </script>
+        <br>
+        <script>
+        let name = "Sahakyan Mariam"; age = "19";
+        console.log("My name is " + name + " " + "I am" + " " + age);
+    </script>
+    <br>
+    <script>
+        document.write("Task 3-");
+        document.write(" ")
+        let x=0;  
+        if (x>=0){
+            document.write("no");
+        }
+        else {
+            document.write("yes");
+        }
+        document.write(" ")
+        let y=-55;
+        if (y>0) {
+            document.write("no");
+        }
+        else {
+            document.write("yes")
+        }
+        document.write(" ")
+        let z=87;
+        if (z>0) {
+            document.write("no")
+        }
+        else {
+            document.write("yes")
+        }
+    </script>
+    <br>
+    <script>
+        document.write("Task 4-")
+        document.write(" ")
+        let s=363;
+        if (s%2==0) {
+            document.write("even")
+        }
+        else {
+            document.write("odd")
+        }
+        document.write(" ")
+        let d=85;
+        if (d%2==0) {
+            document.write("even")
+        }
+        else {
+            document.write("odd")
+        }
+        document.write(" ")
+        let f=60;
+        if (f%2==0) {
+            document.write("even")
+        }
+        else {
+            document.write("odd")
+        }
+    </script>
+    <br>
+    <script>
+        document.write("Task 5-")
+        document.write(" ")
+        let m=3; n=45
+        if (m%n==0){
+            document.write("1")
+        }
+        else{
+            document.write("0")
+        }
+        document.write(" ")
+        let l=18; k=8
+        if (l%k==0){
+            document.write("1")
+        }
+        else{
+            document.write("0")
+        }
+        document.write(" ")
+        let t=25; v=6
+        if (t%v==0){
+            document.write("1")
+        }
+        else{
+            document.write("0")
+        }
+    </script>
+</body>
+</html>
